cb8dc1f8a1905d6c1385dbc59ce557c726417ec6
BocianRocky <mateusz.ro@op.pl>
2025-12-07 15:03:56 +0100

    add changePromotion

---
+++ API/DTOs/UpdatePromotionRequestDto.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class UpdatePromotionRequestDto
+{
+    [Required]
+    [MaxLength(20)]
+    public string PromotionName { get; set; } = string.Empty;
+
+    [Required]
+    public DateOnly DateStart { get; set; }
+
+    [Required]
+    public DateOnly DateEnd { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string BonusType { get; set; } = string.Empty;
+
+    [Range(0, double.MaxValue)]
+    public decimal BonusValue { get; set; }
+
+    [MaxLength(50)]
+    public string? PromoCode { get; set; }
+
+    public decimal? MinDeposit { get; set; }
+
+    public decimal? MaxDeposit { get; set; }
+    
+    [MaxLength(70)]
+    public string Image { get; set; } = string.Empty;
+    
+    public string Description { get; set; } = string.Empty;
+}
+
